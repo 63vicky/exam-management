5957a3771e2e510a5f8111f9e1028983a0851189
63Vicky <6326vivekgangani@gmail.com>
2025-03-19 14:03:36 +0530

    पेपर रूट्स में ऑथेंटिकेशन मिडलवेयर के आयात को अपडेट किया गया।

--- routes/paperRoutes.js
+++ routes/paperRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const paperController = require('../controllers/paperController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 // Apply protect middleware to all routes
 router.use(protect);
