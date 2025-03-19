3654b7001f8512239795da340854b2ea12c56173
63Vicky <6326vivekgangani@gmail.com>
2025-03-19 18:17:31 +0530

    Update import path for authentication middleware in category routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 // Apply protect middleware to all routes
 router.use(protect);
